e00945a48268666c15b6064f22f1953f3e217b5a
anastasiya-liapko <anastasiya.liapko@gmail.com>
2018-04-28 00:39:38 +0300

    Обернула кодом только изменяемую часть

--- index.php
+++ index.php
@@ -97,9 +97,7 @@ $show_complete_tasks = rand(0, 1);
 
                     <label class="checkbox">
                         <!--добавить сюда аттрибут "checked", если переменная $show_complete_tasks равна единице-->
-                        <?php
-                        echo '<input class="checkbox__input visually-hidden show_completed" type="checkbox"'.($show_complete_tasks == 1 ? " checked".">": ">");
-                        ?>
+                        <input class="checkbox__input visually-hidden show_completed" type="checkbox" <?= $show_complete_tasks == 1 ? "checked" : "" ?>>
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
                 </div>
