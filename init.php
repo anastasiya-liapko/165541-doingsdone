0edf34edbf0cf3f0bcf6ecf8292266e0ddd60420
anastasiya-liapko <anastasiya.liapko@gmail.com>
2018-05-29 15:38:45 +0300

    Добавила проверку на существование showCompleteTasks

--- init.php
+++ init.php
@@ -1,3 +1,3 @@
 <?php
-$link = mysqli_connect("localhost", "root", "11111111", "doingsdone-165541");
+$link = mysqli_connect("mysql", "root", "", "doingsdone-165541");
 mysqli_set_charset($link, "utf8");
